907868f4753d9f57e63ebfea8125f78c401ea010
DavidHC9 <davidherreracarvajal1@gmail.com>
2026-05-18 14:48:33 -0500

    actualizacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'home',
+        component: Home
+    },
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
+
+    }
+]
