70ca9ec40faee92b858fcf8563df8c7c9e5404b2
Alejandro <alejobeos05@gmail.com>
2026-05-18 17:01:22 -0500

    problema con el login

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { Auth } from '../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,19 +17,40 @@ export class Login {
     password: new FormControl(''),
   });
 
-  // State to toggle password visibility
+  constructor(private authService: Auth, private router: Router) { }
+
+  // Estado para ocultar la visibilidad de la contraseña
   mostrarContrasena: boolean = false;
 
   toggleMostrarContrasena() {
     this.mostrarContrasena = !this.mostrarContrasena;
   }
 
-  iniciarSesion() {
+  iniciarSesion(): void {
+    let email = this.formularioLogin.value.email || '';
+    let password = this.formularioLogin.value.password || '';
+
+    this.authService.Login(email, password).subscribe(
+      {
+        next: respuesta => {
+          if (respuesta.token) {
+            this.authService.guardarToken(respuesta.token);
+            this.router.navigate(['/home']);
+          }
+        },
+        error: error => {
+          console.error(error);
+        }
+      }
+    )
+  }
+
+  /* iniciarSesion() {
     if (this.formularioLogin.valid) {
       console.log('Datos de inicio de sesión:', this.formularioLogin.value);
       alert(`Iniciando sesión con email: ${this.formularioLogin.value.email}`);
     } else {
       alert('Por favor, complete los campos correctamente.');
     }
-  }
+  } */
 }
